08dc11fdab3d613caadeecf2e5bfcb6154ca29d7
Tsuyoshi Tatsukawa <ts.tatsukawa@gmail.com>
2016-10-15 00:35:20 +0900

    add blocks

--- script.ts
+++ script.ts
@@ -4,35 +4,34 @@ var ScratchExtensions: any
 
 (function(ext: any) {
     var server = new PLENControlServer($);
-    console.log(server)
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
     // Status reporting code
     // Use this to report missing hardware, plugin or unsupported browser
+    ext.connect = function() {
+        server.connect();
+    }
+    
     ext._getStatus = function() {
         return {status: 2, msg: 'Ready'};
     };
 
-    ext.forward = function() {
-        console.log("forward");
-        console.log(server);
+    ext.forward = function(n: number) {
         server.play(70);
     }; 
     
-    ext.connect = function() {
-        console.log("connect");
-        console.log(server);
-        server.connect();
-    }
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             // block type, block name, function name
-            [' ', 'forward', 'forward'],
-            [' ', 'connect', 'connet'],
-            [' ', '止まる', 'stop']
+            [' ', '接続する', 'connet'],
+            [' ', '止まる', 'stop'],
+            [' ', '%n歩動かす', 'forward', 10],
+            [' ', '時計回りに%n度回す', 'right_turn', 15],
+            [' ', '反時計周りに%n度回す', 'left_turn', 15],
+            [' ', '%n度に向ける', 'direct']
         ]
     };
 
